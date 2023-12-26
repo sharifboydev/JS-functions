b0d7498e1e8732808c5242d324e856bd155a9f93
sharifboy <sharifboy@gmail.com>
2023-12-26 15:10:35 +0500

    added new code

---
+++ app.js
@@ -0,0 +1,16 @@
+// function myFirstFunction(str) {
+//     if (str == str.split("").reverse("").join("")) {
+//         console.log("Palindrom");
+//     } else {
+//         console.log("Palindrom emas");
+//     }
+// }
+
+// myFirstFunction("salom")
+
+function finddegree(a) {
+    console.log(Math.sqrt(a));
+}
+
+finddegree(23)
+finddegree(30)
